d4f3a6f0cb1c329e57ca64de7303f860e7a9c01e
jaafar91 <m.jaafar.91@gmail.com>
2022-04-12 22:16:25 +0300

    another-lesson

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import homePageView1,homePageView2
+from .views import homePageView1,homePageView2,HomePageView,AboutPageView
+
 
 urlpatterns = [
-    path("", homePageView1, name="home"),
-    path("about/", homePageView2, name="about"),
+    #path("", homePageView1, name="default"),
+    #path("about/", homePageView2, name="about"),
+    path("", HomePageView.as_view(), name="home"),
+    path("about/", AboutPageView.as_view(), name="about"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 def homePageView1(request):
@@ -7,3 +8,9 @@ def homePageView1(request):
 
 def homePageView2(request):
     return HttpResponse("Hello, Earth!")
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
